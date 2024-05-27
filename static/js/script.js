7eaaf9924edacdedc48f48454c596f9c1c462c6e
Villalba-carla84 <carlavillalba84@hotmail.com>
2024-05-26 23:24:49 -0300

    inicio de sesion

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,21 @@ function iniciarMap(){
       map: map
     });
 }
+
+
+const email= document.getElementById('email');
+const password= document.getElementById('password');
+const button= document.getElementById('button');
+
+button.addEventListener('click',(e)=>{
+e.preventDefault()
+
+const data ={
+  email:email.value,
+  password:password.value
+}
+console.log(data) 
+
+
+
+})
